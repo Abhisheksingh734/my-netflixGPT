9838da9afa1f3c4b12cd98ce4cfd86dd9a957ee4
Abhishek <abhisheksinghasr734@gmail.com>
2023-08-26 16:30:01 +0530

    cra & tailwind

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold">Hello, world</h1>
+    </div>
+  );
+}
+
+// #features
+
+// -Login/Signup
+//   -signup/sign in form
+//   -redirect to browse page
+
+// -Browse(after authentication)
+//   -Header
+//   -main movie
+//     -trailer in bg
+//     -title and description
+//     -movie sugestion
+//         -movielist * N
+
+// -Netflix GPT
+//   -searchBar
+//   -movie suggestions
+
+export default App;
